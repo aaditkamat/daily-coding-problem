6775a52a0528247ec139a46d189c13c2e6efde84
Aadit Kamat <aadit.k12@gmail.com>
2019-06-24 11:31:28 +0800

    Add solution to Problem #173

---
+++ problem_173.py
@@ -0,0 +1,53 @@
+```
+This problem was asked by Stripe.
+
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+```
+def not_a_nested_dict(dictionary):
+    return len(list(filter(lambda x: type(x) == dict, dictionary.values()))) == 0
+
+def flatten(dictionary):
+    if not_a_nested_dict(dictionary):
+        return dictionary
+    new_dictionary = {}
+    for key in dictionary:
+        if not type(dictionary[key]) == dict:
+            new_dictionary[key] = dictionary[key]
+        else:
+            flattened_dictionary = flatten(dictionary[key])
+            for abridged_key in flattened_dictionary:
+                new_dictionary[f"{key}.{abridged_key}"] = flattened_dictionary[abridged_key]
+    return new_dictionary
+    
+
+if __name__ == '__main__':
+    dictionary = {
+                    "key": 3,
+                    "foo": {
+                        "a": 5,
+                        "bar": {
+                            "baz": 8
+                        }
+                     }
+                }
+    print(flatten(dictionary))
